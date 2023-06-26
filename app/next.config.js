4f57562193a9b751a7229d393651a856a91a0c50
Kenji Saito <ken-yo@mbr.nifty.com>
2023-06-26 09:28:17 +0900

    Bumps node modules

--- app/next.config.js
+++ app/next.config.js
@@ -1,21 +1,23 @@
-/** @type {import('next').NextConfig} */
 const withPlugins = require('next-compose-plugins');
 const withBundleAnalyzer = require('@next/bundle-analyzer');
 
+/** @type {import('next').NextConfig} */
+const config = {
+  output: "export",
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    esmExternals: true,
+  },
+  webpack: (config, { isServer }) => {
+    config.experiments = {
+      asyncWebAssembly: true,
+    };
+    config.output.webassemblyModuleFilename = (isServer ? '../' : '') + 'static/wasm/webassembly.wasm';
+    return config;
+  }
+};
+
 module.exports = withBundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
-})({
-    reactStrictMode: true,
-    swcMinify: true,
-    experimental: {
-      esmExternals: true,
-    },
-    webpack: (config, { isServer }) => {
-      config.experiments = {
-        asyncWebAssembly: true,
-      };
-      config.output.webassemblyModuleFilename = (isServer ? '../' : '') + 'static/wasm/webassembly.wasm';
-      return config;
-    }
-  }
-);
+})(config);
