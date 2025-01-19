3a4dd92dd3b9c01b377f59be78a80da099a02698
Kenji Saito <ken-yo@mbr.nifty.com>
2025-01-19 18:45:10 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -21,7 +21,7 @@ if [ $result -ne 0 ]; then
 fi
 echo ""
 pwd
-corepack use pnpm@latest && pnpm install && pnpm up -r && pnpm build
+pnpm install && pnpm up -r && pnpm build
 result=$?
 if [ $result -ne 0 ]; then
   cd "${CUR}"
@@ -45,7 +45,7 @@ if [ $result -ne 0 ]; then
 fi
 echo ""
 pwd
-corepack use pnpm@latest && pnpm install && pnpm up -r
+pnpm install && pnpm up -r
 result=$?
 if [ $result -ne 0 ]; then
   cd "${CUR}"
@@ -61,7 +61,7 @@ if [ $result -ne 0 ]; then
 fi
 echo ""
 pwd
-corepack use pnpm@latest && pnpm install && pnpm up -r
+pnpm install && pnpm up -r
 result=$?
 if [ $result -ne 0 ]; then
   cd "${CUR}"
