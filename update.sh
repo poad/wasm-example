cfb53ed697a5e32425debab2a0cb92e081f73b7a
Kenji Saito <ken-yo@mbr.nifty.com>
2025-02-25 22:44:48 +0900

    Bumps node modules

--- update.sh
+++ update.sh
@@ -2,83 +2,29 @@
 
 CUR=$(pwd)
 
-CURRENT=$(cd $(dirname $0);pwd)
+CURRENT=$(cd "$(dirname "$0")" || exit;pwd)
 echo "${CURRENT}"
 
-cd "${CURRENT}"
+cd "${CURRENT}" || exit
 git pull --prune
 result=$?
 if [ $result -ne 0 ]; then
-  cd "${CUR}"
-  exit $result
-fi
-
-cd "${CURRENT}"/app
-result=$?
-if [ $result -ne 0 ]; then
-  cd "${CUR}"
+  cd "${CUR}" || exit
   exit $result
 fi
 echo ""
 pwd
-npx corepack use pnpm@latest && pnpm install && pnpm up -r && pnpm build
+npx corepack use pnpm@latest && pnpm install && pnpm up -r && pnpm run -r build
 result=$?
 if [ $result -ne 0 ]; then
-  cd "${CUR}"
-  exit $result
-fi
-
-cd "${CURRENT}"
-git pull --prune
-result=$?
-if [ $result -ne 0 ]; then
-  cd "${CUR}"
-  exit $result
-fi
-
-
-cd "${CURRENT}"/infra
-result=$?
-if [ $result -ne 0 ]; then
-  cd "${CUR}"
-  exit $result
-fi
-echo ""
-pwd
-pnpm install && pnpm up -r
-result=$?
-if [ $result -ne 0 ]; then
-  cd "${CUR}"
-  exit $result
-fi
-
-
-cd "${CURRENT}"/infra/function
-result=$?
-if [ $result -ne 0 ]; then
-  cd "${CUR}"
-  exit $result
-fi
-echo ""
-pwd
-pnpm install && pnpm up -r
-result=$?
-if [ $result -ne 0 ]; then
-  cd "${CUR}"
-  exit $result
-fi
-
-cd "${CURRENT}"
-result=$?
-if [ $result -ne 0 ]; then
-  cd "${CUR}"
+  cd "${CUR}" || exit
   exit $result
 fi
 git commit -am "Bumps node modules" && git push
 result=$?
 if [ $result -ne 0 ]; then
-  cd "${CUR}"
+  cd "${CUR}" || exit
   exit $result
 fi
 
-cd "${CUR}"
+cd "${CUR}" || exit
